f17a25a2fa7818f6dfdfb40989d3e195c0d5b588
Sajidimon <imonchowdhury6@gmail.com>
2026-04-28 11:21:39 +0600

    setup sanctum for secure api

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('services', ServiceController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('services', ServiceController::class);
+});
